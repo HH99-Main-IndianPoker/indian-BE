eb37f4d7e53d1b2bfca035a303c9c1f85b4c6aaf
lsc713 <lsc7134.naver.com>
2024-04-02 19:20:37 +0900

    build(): init commit
    indian_frog backend init commit

---
+++ src/main/java/com/service/indianfrog/IndianfrogApplication.java
@@ -0,0 +1,13 @@
+package com.service.indianfrog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndianfrogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IndianfrogApplication.class, args);
+    }
+
+}
