ccb765aa7de468403d4962903ab11ee393212074
lsc713 <lsc7134.naver.com>
2024-04-08 19:30:34 +0900

    fix: update gameplay batting
    update gameplay batting
    TODO : PASSWORD SETTING,EXCEPTION

--- src/test/java/com/service/indianfrog/IndianfrogApplicationTests.java
+++ src/test/java/com/service/indianfrog/IndianfrogApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class IndianfrogApplicationTests {
+class IndianFrogApplicationTests {
 
     @Test
     void contextLoads() {
