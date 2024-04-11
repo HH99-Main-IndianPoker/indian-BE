e00c83c56b44bcd39c8e90d363257387b11c9b93
kjs4231 <kjs4231@naver.com>
2024-04-11 12:11:07 +0900

    fix: gameroom nickname 저장 #53
    기존의 이메일이 아닌 닉네임을 저장하도록 수정

--- src/main/java/com/service/indianfrog/domain/gameroom/service/GameRoomService.java
+++ src/main/java/com/service/indianfrog/domain/gameroom/service/GameRoomService.java
@@ -63,7 +63,6 @@ public class GameRoomService {
         return gameRoomRepository.existsById(roomId);
     }
 
-
     @PostConstruct
     public void init() {
         try {
@@ -81,17 +80,18 @@ public class GameRoomService {
         return (message == null || message.trim().isEmpty()) ? message : pattern.matcher(message).replaceAll("**");
     }
 
-
     @Transactional
     public GameRoomCreateResponseDto createGameRoom(GameRoomCreateRequestDto gameRoomDto, Principal principal) {
         String email = principal.getName();
-        userRepository.findByEmail(email).orElseThrow(() -> new RestApiException(ErrorCode.NOT_FOUND_USER.getMessage()));
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new RestApiException(ErrorCode.NOT_FOUND_USER.getMessage()));
+        String nickname = user.getNickname();
 
         LocalDateTime now = LocalDateTime.now();
         GameRoom savedGameRoom = gameRoomRepository.save(gameRoomDto.toEntity());
 
         ValidateRoom validateRoom = new ValidateRoom();
-        validateRoom.setParticipants(email);
+        validateRoom.setParticipants(nickname);
         validateRoom.setGameRoom(savedGameRoom);
         validateRoom.setHost(true);
         validateRoomRepository.save(validateRoom);
@@ -99,12 +99,12 @@ public class GameRoomService {
         return new GameRoomCreateResponseDto(savedGameRoom.getRoomId(), savedGameRoom.getRoomName(), now);
     }
 
-
     @Transactional
     public ValidateRoomDto addParticipant(Long roomId, Principal participant) {
         String email = participant.getName();
-        userRepository.findByEmail(email)
+        User user = userRepository.findByEmail(email)
                 .orElseThrow(() -> new RestApiException(ErrorCode.NOT_FOUND_USER.getMessage()));
+        String nickname = user.getNickname();
 
         GameRoom gameRoom = gameRoomRepository.findById(roomId)
                 .orElseThrow(() -> new RestApiException(ErrorCode.NOT_FOUND_GAME_ROOM.getMessage()));
@@ -113,12 +113,12 @@ public class GameRoomService {
             throw new RestApiException(ErrorCode.GAME_ROOM_NOW_FULL.getMessage());
         }
 
-        if (validateRoomRepository.findByGameRoomAndParticipants(gameRoom, email).isPresent()) {
+        if (validateRoomRepository.findByGameRoomAndParticipants(gameRoom, nickname).isPresent()) {
             throw new RestApiException(ErrorCode.ALREADY_EXIST_USER.getMessage());
         }
 
         ValidateRoom validateRoom = new ValidateRoom();
-        validateRoom.setParticipants(email);
+        validateRoom.setParticipants(nickname);
         validateRoom.setGameRoom(gameRoom);
         validateRoom = validateRoomRepository.save(validateRoom);
 
@@ -128,33 +128,29 @@ public class GameRoomService {
     @Transactional
     public GetGameRoomResponseDto removeParticipant(Long roomId, Principal participant) {
         String email = participant.getName();
-
-        // 사용자가 존재하는지 확인 후, 존재하지 않으면 예외를 던집니다.
         User user = userRepository.findByEmail(email)
                 .orElseThrow(() -> new RestApiException(ErrorCode.NOT_FOUND_USER.getMessage()));
+        String nickname = user.getNickname();
 
-        // 사용자가 해당 방에 참여하고 있는지 확인
-        List<ValidateRoom> validateRooms = validateRoomRepository.findAllByGameRoomRoomIdAndParticipants(roomId, email);
+        List<ValidateRoom> validateRooms = validateRoomRepository.findAllByGameRoomRoomIdAndParticipants(roomId, nickname);
         if (validateRooms.isEmpty()) {
             throw new RestApiException(ErrorCode.GAME_USER_HAS_GONE.getMessage());
         }
+
         boolean wasHost = validateRooms.stream().anyMatch(ValidateRoom::isHost);
         validateRooms.forEach(validateRoomRepository::delete);
 
         if (wasHost) {
-            // 방장이 방을 나가면 새 방장을 지정
             List<ValidateRoom> remainParticipant = validateRoomRepository.findAllByGameRoomRoomId(roomId);
             if (!remainParticipant.isEmpty()) {
-                ValidateRoom newHost = remainParticipant.get(0); //남아있는 첫번째 참가자를 방장으로 지정, 만약 참가자가 여러명이면 거의 랜덤?
+                ValidateRoom newHost = remainParticipant.get(0);
                 newHost.setHost(true);
                 validateRoomRepository.save(newHost);
             }
         }
 
-        //방이 비었는지 검사해서 없으면 방 없애버림.
         boolean isRoomEmpty = !validateRoomRepository.existsByGameRoomRoomId(roomId);
         if (isRoomEmpty) {
-            // 방이 비었으므로 삭제하고 null 반환
             gameRoomRepository.deleteById(roomId);
             return null;
         }
@@ -165,23 +161,18 @@ public class GameRoomService {
         return new GetGameRoomResponseDto(gameRoom.getRoomId(), gameRoom.getRoomName());
     }
 
-
     @Transactional
     public void removeParticipantBySessionId(String sessionId) {
-        String email = sessionMappingStorage.getEmailBySessionId(sessionId);
-        if (email == null) {
-            // 세션 ID에 해당하는 이메일을 찾을 수 없음
+        String nickname = sessionMappingStorage.getNicknameBySessionId(sessionId);
+        if (nickname == null) {
             return;
         }
 
-        // 여기서는 예제로, 모든 게임방에서 해당 이메일을 가진 참가자를 제거하는 로직을 구현합니다.
-        List<ValidateRoom> validateRooms = validateRoomRepository.findAllByParticipants(email);
+        List<ValidateRoom> validateRooms = validateRoomRepository.findAllByParticipants(nickname);
         for (ValidateRoom validateRoom : validateRooms) {
             validateRoomRepository.delete(validateRoom);
-            // 추가 로직: 방장 변경, 게임방 삭제 등
         }
 
-        // 세션 매핑에서 제거
         sessionMappingStorage.removeSession(sessionId);
     }
 
