5a36b4cc6fc910ad1d9e1ebcfccbd35623b26c4f
weiß Yi <78130010+Rosa-Eau@users.noreply.github.com>
2024-04-16 17:05:08 +0900

    Fix/start (#76)
    * fix: GameInfo 수정
    
    * feat: StartGameService 수정
    
    sendToUser 구분해서 하나만 보내게 수정

--- src/main/java/com/service/indianfrog/domain/game/entity/Turn.java
+++ src/main/java/com/service/indianfrog/domain/game/entity/Turn.java
@@ -1,15 +1,17 @@
 package com.service.indianfrog.domain.game.entity;
 
 import com.service.indianfrog.domain.user.entity.User;
+import lombok.Getter;
 
 import java.util.List;
 
+@Getter
 public class Turn {
-    private List<User> players;
+    private List<String> players;
     private int currentPlayer;
 
     public Turn(List<User> players) {
-        this.players = players;
+        this.players = players.stream().map(User::getNickname).toList();
         this.currentPlayer = 0;
     }
 
@@ -17,7 +19,7 @@ public class Turn {
         currentPlayer = (currentPlayer + 1) % players.size();
     }
 
-    public User getCurrentPlayer() {
+    public String getCurrentPlayer() {
         return players.get(currentPlayer);
     }
 }
