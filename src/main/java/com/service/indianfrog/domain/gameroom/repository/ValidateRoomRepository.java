9fa35029f53dbf0802b134ff64006081e3a34657
kjs4231 <kjs4231@naver.com>
2024-04-21 03:56:45 +0900

    fix: querydsl 원복

--- src/main/java/com/service/indianfrog/domain/gameroom/repository/ValidateRoomRepository.java
+++ src/main/java/com/service/indianfrog/domain/gameroom/repository/ValidateRoomRepository.java
@@ -1,9 +1,27 @@
 package com.service.indianfrog.domain.gameroom.repository;
 
+import com.service.indianfrog.domain.gameroom.entity.GameRoom;
 import com.service.indianfrog.domain.gameroom.entity.ValidateRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ValidateRoomRepository extends JpaRepository<ValidateRoom, Long>, CustomValidateRoomRepository {
+import java.util.List;
+import java.util.Optional;
+
+public interface ValidateRoomRepository extends JpaRepository<ValidateRoom, Long> {
+
+    Optional<ValidateRoom> findByGameRoomAndParticipants(GameRoom gameRoom, String participant);
+
+    List<ValidateRoom> findAllByGameRoomRoomId(Long roomId);
+
+    List<ValidateRoom> findAllByReadyTrue();
+
+    List<ValidateRoom> findAllByParticipants(String email);
+
+    ValidateRoom findByGameRoomRoomIdAndParticipants(Long roomId, String nickname);
+
+    ValidateRoom findByGameRoomRoomId(Long roomId);
+
+    int countByGameRoomRoomId(Long roomId);
 
 }
 
