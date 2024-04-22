e750313da02ad80998cbc4a7e36b7b9cea67ab26
weiß <78130010+rosa-eau@users.noreply.github.com>
2024-04-22 23:31:03 +0900

    fix: 랭킹 수정 #106

--- src/main/java/com/service/indianfrog/domain/ranking/service/RankingService.java
+++ src/main/java/com/service/indianfrog/domain/ranking/service/RankingService.java
@@ -10,6 +10,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 @Service
@@ -24,28 +25,24 @@ public class RankingService {
     @Transactional(readOnly = true)
     public GetRankingInfo getRanking(String username) {
 
-        List<User> userList = userRepository.findAll();
+        List<Object[]> results = userRepository.findUsersWithRank();
 
-        userList.sort(new Comparator<User>() {
-            @Override
-            public int compare(User o1, User o2) {
-                return Integer.compare(o2.getPoints(), o1.getPoints());
-            }
-        });
-
-        List<GetRanking> rankings = IntStream.range(0, userList.size())
-                .limit(100)
-                .mapToObj(index -> new GetRanking(
-                        index + 1,
-                        userList.get(index).getNickname(),
-                        userList.get(index).getPoints()))
+        List<GetRanking> rankings = results.stream()
+                .map(result -> new GetRanking(
+                        (String) result[0], // imageUrl
+                        ((Long) result[1]).intValue(), // ranking
+                        (String) result[2], // nickname
+                        (Integer) result[3]  // points
+                ))
                 .toList();
 
         User user = userRepository.findByEmail(username).orElseThrow(() -> new RestApiException(ErrorCode.NOT_FOUND_USER.getMessage()));
 
+        List<User> userList = userRepository.findAll();
+
         int myRanking = IntStream.range(0, userList.size()).filter(i -> userList.get(i).getEmail().equals(username)).findFirst().orElseThrow(() -> new RestApiException(ErrorCode.NOT_FOUND_EMAIL.getMessage())) + 1;
 
-        return new GetRankingInfo(rankings, user.getNickname(), myRanking, user.getPoints());
+        return new GetRankingInfo(rankings, user.getNickname(), user.getImageUrl(),myRanking, user.getPoints());
 
     }
 
