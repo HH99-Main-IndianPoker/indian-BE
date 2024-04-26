6b732af7d636c264ec8979db9769682a1dbfeade
weiß Yi <78130010+Rosa-Eau@users.noreply.github.com>
2024-04-27 01:55:41 +0900

    Fix/ranking (#145)
    * fix: game 여러개 생성 #138
    
    * fix: 랭킹 순위 중복값 순차 적용 #144

--- src/main/java/com/service/indianfrog/domain/user/repository/UserRepository.java
+++ src/main/java/com/service/indianfrog/domain/user/repository/UserRepository.java
@@ -1,6 +1,5 @@
 package com.service.indianfrog.domain.user.repository;
 
-import com.service.indianfrog.domain.ranking.dto.Ranking.*;
 import com.service.indianfrog.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -11,11 +10,10 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
 
 
-    @Query(value = "SELECT infp.user.image_url, DENSE_RANK() OVER (ORDER BY points DESC) AS ranking, nickname, points " +
+    @Query(value = "SELECT infp.user.image_url, ROW_NUMBER() OVER (ORDER BY points DESC) AS ranking, nickname, points " +
             "FROM infp.user " +
             "LIMIT 100", nativeQuery = true)
     List<Object[]> findUsersWithRank();
-
 
     Optional<User> findByEmail(String email);
 
