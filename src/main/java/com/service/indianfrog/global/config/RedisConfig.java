7a1488ee8ef2bee879c8f1077c6c9e298ace322a
Weiß <tory020900@gmail.com>
2025-01-08 12:05:53 +0900

    deploy: 빌드 에러 수정

--- src/main/java/com/service/indianfrog/global/config/RedisConfig.java
+++ src/main/java/com/service/indianfrog/global/config/RedisConfig.java
@@ -42,4 +42,13 @@ public class RedisConfig {
         redisTemplate.setValueSerializer(new StringRedisSerializer());
         return redisTemplate;
     }
+
+    @Bean
+    public RedisTemplate<String, Object> objectRedisTemplate() {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+        return redisTemplate;
+    }
 }
